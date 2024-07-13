9521059d69e833f95d77ada125f6b907b8e7ac1f
Javieraaa <javieraalejandramh@gmail.com>
2024-07-13 17:29:27 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,104 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1>Tienda 32 Bits</h1>
+    <p>Lista de videojuegos</p>
+    <table>
+      <thead>
+        <tr>
+          <th>Código</th>
+          <th>Nombre</th>
+          <th>Stock</th>
+          <th>Precio</th>
+          <th>Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="juego in allGames" :key="juego.codigo">
+          <td>{{ juego.codigo }}</td>
+          <td>{{ juego.nombre }}</td>
+          <td>{{ juego.stock }}</td>
+          <td>${{ juego.precio }}</td>
+          <td>
+            <button @click="decrementGamesStock(juego.codigo)"> - </button>
+            <button @click="incrementGamesStock(juego.codigo)"> + </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { mapGetters, mapActions } from 'vuex';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  computed: {
+    ...mapGetters(['allGames'])
+  },
+  methods: {
+    ...mapActions(['fetchGames', 'incrementGamesStock', 'decrementGamesStock']),
+    
+    loadGames(){
+      this.fetchGames();
+    }
+  },
+  mounted() {
+    this.loadGames()
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  #app {
+    background: linear-gradient(135deg, #fa5bbd, #dfc2d0);
+    display: flex;
+    flex-direction: column;
+    align-items: center; 
+    border-radius: 15px;
+    font-family: 'Roboto';
+  }
+
+  .container {
+    padding: 10rem;
+    margin: 5rem;
 }
+
+  
+  h1, p {
+    text-align: center;
+  }
+  
+  table {
+    width: 80%; 
+    border-collapse: collapse; 
+    margin-top: 20px; 
+    border: 2px solid #ccc; 
+  }
+  
+  th, td {
+    border: 1px solid #ccc; 
+    padding: 8px; 
+    text-align: center; 
+  }
+  
+  th {
+    background-color: #916791; 
+  }
+  
+  tbody tr:nth-child(even) {
+    background-color: #ac6998; 
+  }
+  
+  tbody tr:hover {
+    background-color: #eca5d1; 
+  }
+  
+  button{
+    margin: .1rem;
+    width: 1.5rem;
+    border: none;
+    border-radius: 7px;
+    background-color: rgb(255, 199, 243);
+  }
+
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,44 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+state: {
+    games: []
+},
+getters: {
+    allGames: (state) => state.games,
+},
+mutations: {
+    setGames(state, games) {
+        state.games = games
+    },
+    decrementGamesStock(state, gameId) {
+        const gameSelected = state.games.find(item => item.codigo === gameId)
+        if(gameSelected && gameSelected.stock > 0) {
+            gameSelected.stock--
+        }
+    },
+    incrementGamesStock(state, gameId) {
+        const gameSelected = state.games.find(item => item.codigo === gameId)
+        if(gameSelected) {
+            gameSelected.stock++
+        }
+    }
+},
+actions: {
+    async fetchGames({ commit }) {
+        try {
+            const { data } = await axios.get('/api/juegos.json')
+            commit('setGames', data)
+        }catch(error) {
+            console.error('No pudimos tomar la data', error)
+        }
+    },
+    async incrementGamesStock({ commit }, gameId) {
+        commit('incrementGamesStock', gameId)
+    },
+    async decrementGamesStock({ commit }, gameId) {
+        commit('decrementGamesStock', gameId)
+    }
+}
 })
